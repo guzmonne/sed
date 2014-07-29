af16327b32952c16a17449569a5169c90c74cbc6
Guzman Monne <guzmonne@hotmail.com>
2014-07-29 10:13:40 -0300

    moved login logic to main and deleted login foldre

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,4 +9,22 @@ angular.module('sedApp')
     $scope.userName = function(){
       return Auth.getCurrentUser().name;
     };
+    $scope.user   = {};
+    $scope.errors = {};
+    $scope.pattern = new RegExp(/@/);
+    $scope.login  = function(form) {
+      $scope.submitted = true;
+      if(form.$valid) {
+        Auth.login({
+          email   : $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          $location.path('/');
+        })
+        .catch( function(err) {
+          $scope.errors.other = err.message;
+        });
+      }
+    };
   }]);
