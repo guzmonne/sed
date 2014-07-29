a9f72de9991fc494d76dfe7cdef12d797ab7940d
Guzman Monne <guzmonne@hotmail.com>
2014-07-28 21:13:41 -0300

    created client form directive

---
+++ client/app/client/directives/clientForm/clientForm.directive.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('sedApp')
+  .directive('clientForm', function () {
+    return {
+      templateUrl: 'app/client/directives/clientForm/clientForm.html',
+      restrict: 'AE',
+      scope: {
+				model  : '=',
+				success: '&',
+				error  : '&',
+      },
+      controller : ['$scope', 'ClientModel', 'Alerts', function($scope, ClientModel, Alerts){
+      	/*
+      	** Public
+        */
+      	$scope.docTypes   = ClientModel.docTypes;
+      	$scope.alerts     = [];
+      	$scope.closeAlert = Alerts.closeAlert; 
+      	$scope.submit     = function(){ if ($scope.model._id){ submit('update'); } else { submit('create'); } };
+      	/*
+      	** Private
+      	*/
+      	var defaults = ClientModel.empty();
+      	function errorHandler(error){
+      		Alerts.pushAlert($scope.alerts, {type: 'danger', msg: 'Ya existe un cliente con este documento o ha ocurrido un error en el servidor'});
+      		if (_.isFunction($scope.error)){ $scope.error(error); }
+      	};
+      	function successHandler(data){
+      		var msg;
+      		if (model._id){ 
+      			msg = 'Cliente creado con exito!'; 
+      			$scope.model = ClientModel.empty();
+      		} else { 
+      			msg = 'Cliente actualizado con exito!'; 
+      		}
+      		$scope.clientNewForm.$setPristine();
+      		if (_.isFunction($scope.success)){ $scope.success(data); }
+      	};
+      	function submit(method){
+      		ClientModel[method]($scope.model).then(successHandler, errorHandler);
+      	};
+      }],
+    };
+  });
