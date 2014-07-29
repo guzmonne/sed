e36d039f8db94a222eb8e5bb27ae5529bd3c2156
Guzman Monne <guzmonne@hotmail.com>
2014-07-29 14:15:51 -0300

    moved login logic to main

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -2,22 +2,5 @@
 
 angular.module('sedApp')
   .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
-    $scope.user   = {};
-    $scope.errors = {};
-    $scope.pattern = new RegExp(/@/);
-    $scope.login  = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.login({
-          email   : $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          $location.path('/');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
-      }
-    };
+
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,12 +1,30 @@
 'use strict';
 
 angular.module('sedApp')
-  .controller('MainCtrl', ['$scope', 'Auth', function ($scope, Auth) {
+  .controller('MainCtrl', ['$scope', '$location', 'Auth', function ($scope, $location, Auth) {
     $scope.notLoggedIn = function(){
       return !Auth.isLoggedIn();
     };
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.userName = function(){
+		$scope.isLoggedIn = Auth.isLoggedIn;
+		$scope.userName   = function(){
       return Auth.getCurrentUser().name;
     }
+		$scope.user    = {};
+		$scope.errors  = {};
+		$scope.pattern = new RegExp(/@/);
+		$scope.login   = function(form) {
+      $scope.submitted = true;
+      if(form.$valid) {
+        Auth.login({
+          email   : $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          $location.path('/');
+        })
+        .catch( function(err) {
+          $scope.errors.other = err.message;
+        });
+      }
+    };
   }]);
