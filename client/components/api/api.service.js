2ef656bbd64010074bf2a000ce1862e2e9851657
Guzman Monne <guzmonne@hotmail.com>
2014-07-30 12:02:40 -0300

    created ApiFactory

---
+++ client/components/api/api.service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('sedApp')
+  .factory('Api', ['$resource', function($resource){
+    var restRoutes =       {
+      'create':  { method: 'POST' },
+      'index':   { method: 'GET', isArray: true },
+      'show':    { method: 'GET', isArray: false },
+      'update':  { method: 'PUT' },
+      'destroy': { method: 'DELETE' }
+    };
+    return {
+      'Client': $resource('/api/clients/:id', {id: '@_id'}, restRoutes);
+    }
+  }]
+);
