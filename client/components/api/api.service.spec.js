381a3c22659961face881fb794aba480875a604f
Guzman Monne <guzmonne@hotmail.com>
2014-07-30 15:52:10 -0300

    created a mock test to avoid build errors

---
+++ client/components/api/api.service.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Service: Api', function () {
+
+  // load the controller's module
+  beforeEach(module('sedApp'));
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});
